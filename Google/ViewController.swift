c04c24563df8f92c18c984fea9d28eb029c247ec
trueheartedly <trueheartedly@gmail.com>
2015-09-16 21:46:43 -0700

    Initial Commit

---
+++ Google/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Google
+//
+//  Created by James Taylor on 9/15/15.
+//  Copyright (c) 2015 James Taylor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+}
+
